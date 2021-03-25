de9ae73b47b64f0b4429c6d01e370a9d58cca6bd
Samira Mahdavi <samiramahdavii@gmail.com>
2021-03-26 00:38:22 +0430

    Add Class

---
+++ designpatterns.singletone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.SingleTone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
